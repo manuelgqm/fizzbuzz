3e43b22aea72fe06b9899f9aec3274ca27caa031
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2017-11-27 18:34:10 +0100

    completes rule handler class try

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,32 +1,37 @@
-class FizzBuzz
-  FIZZ = 'fizz'
-  BUZZ = 'buzz'
-  FIZZBUZZ = FIZZ + BUZZ
-  FIZZ_MULTIPLE = 3
-  BUZZ_MULTIPLE = 5
+FIZZ_NUMBER = 3
+BUZZ_NUMBER = 5
+MAX_NUMBER = 100
 
-  def values
-    (1..100).map { |item| convert(item) }
+class FizzbuzzRules
+  def fizzbuzz(arg)
+    fizz = fizz(arg)
+    buzz = buzz(arg)
+    return fizz.to_s + buzz.to_s if fizz || buzz
+    arg
   end
 
-  def convert(number)
-    return FIZZBUZZ if fizzbuzz?(number)
-    return FIZZ if fizz?(number)
-    return BUZZ if buzz?(number)
-    number
+  def fizz(arg)
+    "Fizz" if arg % FIZZ_NUMBER == 0 || arg.to_s.include?(FIZZ_NUMBER.to_s)
   end
 
-  def fizz?(number)
-    number%FIZZ_MULTIPLE == 0 || number.to_s.include?(FIZZ_MULTIPLE.to_s)
+  def buzz(arg)
+    "Buzz" if arg % BUZZ_NUMBER == 0 || arg.to_s.include?(BUZZ_NUMBER.to_s)
+  end
+end
+
+class Fizzbuzz < FizzbuzzRules
+  attr_reader :numbers
+
+  def initialize
+    @numbers = (0..MAX_NUMBER).to_a
+    @numbers = @numbers.map { |number| fizzbuzz(number) }
   end
 
-  def buzz?(number)
-    number%BUZZ_MULTIPLE == 0
-  end
-
-  def fizzbuzz?(number)
-    fizz?(number) && buzz?(number)
+  def to_s
+    @numbers.each { |number| puts number }
   end
 
 end
 
+fizzbuzz = Fizzbuzz.new
+fizzbuzz.to_s
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,39 +1,52 @@
-require "fizzbuzz"
+require 'fizzbuzz'
 
-describe 'Fizz buzz' do
-  before(:each) do
-    @fizz = FizzBuzz.new
+describe 'Fizz Buzz' do
+  context 'conversion returns' do
+    before(:each) do
+      @rules = FizzbuzzRules.new
+    end
+
+    it "Fizz if number is divisible by #{FIZZ_NUMBER} or if it has a #{FIZZ_NUMBER} in it" do
+      expect(@rules.fizzbuzz(FIZZ_NUMBER)).to eq("Fizz")
+      expect(@rules.fizzbuzz(FIZZ_NUMBER*2)).to eq("Fizz")
+      expect(@rules.fizzbuzz(FIZZ_NUMBER*3)).to eq("Fizz")
+      expect(@rules.fizzbuzz(13)).to eq("Fizz")
+      expect(@rules.fizzbuzz(31)).to eq("Fizz")
+      expect(@rules.fizzbuzz(32)).to eq("Fizz")
+    end
+
+    it "Buzz if number is divisible by #{BUZZ_NUMBER} or if it has a #{BUZZ_NUMBER} in it" do
+      expect(@rules.fizzbuzz(BUZZ_NUMBER)).to eq("Buzz")
+      expect(@rules.fizzbuzz(BUZZ_NUMBER*2)).to eq("Buzz")
+    end
+
+    it "FizzBuzz if number is divisible by #{FIZZ_NUMBER} and #{BUZZ_NUMBER}" do
+      expect(@rules.fizzbuzz(FIZZ_NUMBER*BUZZ_NUMBER)).to eq("FizzBuzz")
+      expect(@rules.fizzbuzz(FIZZ_NUMBER*BUZZ_NUMBER*2)).to eq("FizzBuzz")
+      expect(@rules.fizzbuzz(52)).to eq("Buzz")
+      expect(@rules.fizzbuzz(59)).to eq("Buzz")
+    end
+
+    it "Number if number is not dibisible by #{FIZZ_NUMBER} or #{BUZZ_NUMBER}" do
+      expect(@rules.fizzbuzz(1)).to eq(1)
+      expect(@rules.fizzbuzz(2)).to eq(2)
+      expect(@rules.fizzbuzz(44)).to eq(44)
+    end
+
   end
 
-  it 'returns a list of 100 numbers' do
-    expect(@fizz.values.length).to eq(100)
+  it "has a list of numbers from 1 to #{MAX_NUMBER}" do
+    fizzbuzz = Fizzbuzz.new
+    expect(fizzbuzz.numbers.size).to eq(MAX_NUMBER + 1)
+    expect(fizzbuzz.numbers[4]).to eq(4)
   end
 
-  it 'returns fizz if number is divisible by fizz' do
-    expect(@fizz.convert(FizzBuzz::FIZZ_MULTIPLE*1)).to eq(FizzBuzz::FIZZ)
-    expect(@fizz.convert(FizzBuzz::FIZZ_MULTIPLE*2)).to eq(FizzBuzz::FIZZ)
-  end
+  it "numbers are converted to fizzbuzz" do
+    fizzbuzz = Fizzbuzz.new
 
-  it 'returns buzz if number is divisible by buzz' do
-    expect(@fizz.convert(FizzBuzz::BUZZ_MULTIPLE*1)).to eq(FizzBuzz::BUZZ)
-    expect(@fizz.convert(FizzBuzz::BUZZ_MULTIPLE*2)).to eq(FizzBuzz::BUZZ)
+    expect(fizzbuzz.numbers[1]).to eq(1)
+    expect(fizzbuzz.numbers[FIZZ_NUMBER]).to eq("Fizz")
+    expect(fizzbuzz.numbers[BUZZ_NUMBER]).to eq("Buzz")
+    expect(fizzbuzz.numbers[FIZZ_NUMBER*BUZZ_NUMBER]).to eq("FizzBuzz")
   end
-
-  it 'returns fizzbuzz if number is divisible by fizz and buzz' do
-    fizzbuzz = FizzBuzz::FIZZ_MULTIPLE * FizzBuzz::BUZZ_MULTIPLE
-    expect(@fizz.convert(fizzbuzz)).to eq(FizzBuzz::FIZZBUZZ)
-  end
-
-  it 'returns number if number is not fizz or buzz multiples' do
-    expect(@fizz.convert(1)).to eq(1)
-  end
-
-  it 'returns converted list' do
-    expect(@fizz.values[2]).to eq(FizzBuzz::FIZZ)
-  end
-
-  it 'returns fizz if number contains fizz multiple' do
-    expect(@fizz.convert(13)).to eq('fizz')
-  end
-
 end
